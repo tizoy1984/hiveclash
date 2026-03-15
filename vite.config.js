980c041165bc765c17f57fd3f5ada40ced14350d
Marius Barends <marius.werk@gmail.com>
2026-03-15 19:52:27 +0100

    Allowing Railway host in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['hiveclash-production.up.railway.app']
+  }
 })
